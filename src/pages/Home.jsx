18a622cffc11ce9470869f7a71b8ce58948c2f33
pxperfectmike <pxperfectmike@gmail.com>
2023-05-15 11:10:25 -0700

    add quilted pics and fix title to the top

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,23 @@ function Home() {
 			sx={{
 				width: '100%',
 				height: '100%',
-				paddingTop: '5%',
 				backgroundColor: '#070606',
+				position: 'relative',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
 			}}
 		>
-			<Box>
+			<Box
+				sx={{
+					position: 'fixed',
+					width: '100%',
+					zIndex: 999,
+					backgroundColor: '#070606',
+					padding: '1rem',
+					height: 'fit-content', // Set a height to ensure it remains fixed
+				}}
+			>
 				<Typography
 					variant='h4'
 					sx={{
@@ -24,6 +36,8 @@ function Home() {
 				>
 					Artist VS Algorithm
 				</Typography>
+			</Box>
+			<Box sx={{ height: 'auto' }}>
 				<HomeCard />
 			</Box>
 		</Container>
