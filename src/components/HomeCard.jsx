4ebecf2772b4934c70d93785e6fb19ca9240bdb3
Michael Baker <56899037+PxPerfectMike@users.noreply.github.com>
2023-09-28 18:04:43 -0700

    add api call to contentful and make it work for now

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Dialog, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Zoom from '@mui/material/Zoom';
+import { createClient } from 'contentful';
+
+// Initialize Contentful client
+const client = createClient({
+	space: '9yyxpwjpu8ht',
+	environment: 'master', // defaults to 'master' if not set
+	accessToken: 'r2_gghUds_gCqXDe6eSc7_o9vhKQ8iplFRsWNZU_UU4',
+});
 
 function srcset(image, size, rows = 1, cols = 1) {
 	return {
@@ -21,6 +29,25 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 export default function HomeCard() {
 	const theme = useTheme();
 	const [selectedImage, setSelectedImage] = useState(null);
+	const [itemData, setItemData] = useState([]);
+
+	useEffect(() => {
+		client
+			.getEntry('2HAYg2vt4vsi8BZOyahFvA')
+			.then((entry) => {
+				const initialItemData = [
+					{
+						type: entry.fields.type,
+						text: entry.fields.homePost,
+						title: entry.fields.title,
+						rows: entry.fields.rows,
+						cols: entry.fields.columns,
+					},
+				];
+				setItemData(initialItemData);
+			})
+			.catch(console.error);
+	}, []);
 
 	const handleImageClick = (image) => {
 		setSelectedImage(image);
@@ -66,7 +93,11 @@ export default function HomeCard() {
 							<Typography
 								variant='h6'
 								color='primary'
-								sx={{ padding: '1rem' }}
+								sx={{
+									padding: '1rem',
+									width: '100vw',
+									textAlign: 'center',
+								}}
 							>
 								{item.text}
 							</Typography>
@@ -118,77 +149,3 @@ export default function HomeCard() {
 		</>
 	);
 }
-
-const itemData = [
-	{
-		type: 'text',
-		text: 'This is where the home page text goes.',
-		title: 'Home page description',
-		rows: 2,
-		cols: 4,
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1654541696896-bd5d8feed3fb',
-		title: 'lighthouse',
-		rows: 2,
-		cols: 4,
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-		title: 'Breakfast',
-		rows: 2,
-		cols: 2,
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-		title: 'Burger',
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-		title: 'Camera',
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-		title: 'Coffee',
-		cols: 2,
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-		title: 'Hats',
-		cols: 2,
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-		title: 'Honey',
-		author: '@arwinneil',
-		rows: 2,
-		cols: 2,
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-		title: 'Basketball',
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-		title: 'Fern',
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-		title: 'Mushrooms',
-		rows: 2,
-		cols: 2,
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-		title: 'Tomato basil',
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-		title: 'Sea star',
-	},
-	{
-		img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-		title: 'Bike',
-		cols: 2,
-	},
-];
